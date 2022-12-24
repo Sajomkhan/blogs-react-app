3406070c46488496a3bfacd9833126f47a91476c
Sajomkhan <sajomkhan@gmail.com>
2022-12-25 00:16:14 +0600

    contact page created
    Signed-off-by: Sajomkhan <sajomkhan@gmail.com>

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import "./contact.css"
 
 function Contact() {
   return (
-    <div>
-        <h1>Contact</h1>
+    <div className='contact'>     
+        
+        <h1>Contact Us</h1>
+        <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa excepturi accusantium soluta saepe labore temporibus ad commodi magni maxime architecto? Modi tempora eos, molestias molestiae quo obcaecati dolorum ullam? Omnis enim excepturi tempore molestias voluptate?</h4>
+        <h5>OFFICE:</h5>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa excepturi accusantium soluta saepe labore temporibus ad commodi magni maxime architecto? Modi tempora eos. </p>
+        <h5>INDUSTRIS:</h5>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa excepturi accusantium soluta saepe labore temporibus ad commodi magni maxime architecto? Modi tempora eos.</p>
+        <h5>Email & Contact:</h5>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Culpa excepturi accusantium soluta saepe labore temporibus ad commodi magni maxime architecto? Modi tempora eos.</p>
+        
     </div>
   )
 }
